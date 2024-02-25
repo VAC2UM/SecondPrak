fdfe7800484aaf43ce377f30896c16efb2531c59
VAC2UM <nikita.golowanev@gmail.com>
2024-02-25 23:18:22 +0300

    Исправлена ошибка с картинкой

--- app/src/main/java/com/itproger/secondprak/MainActivity.java
+++ app/src/main/java/com/itproger/secondprak/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // Инициализируем значениями по умолчанию
         }
+        setContentView(R.layout.activity_main);
     }
 
     private void setUpActionBar(){
